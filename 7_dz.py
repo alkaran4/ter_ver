5f6e36d57adea4a6b045787fcc349e16c9b0e05c
alkaran4 <83422829+alkaran4@users.noreply.github.com>
2022-06-30 19:31:30 +0300

    Update 7_dz.py

--- 7_dz.py
+++ 7_dz.py
@@ -21,8 +21,8 @@ zp = zp.reshape((10,1))
 ks = ks.reshape((10,1))
 
 # zp = np.hstack([np.ones((10,1)), zp])
-b = np.linalg.inv(zp.T@zp)@zp.T@ks
-print(b)
+b = np.linalg.inv(zp.T@zp)@zp.T@ks # 2.62
+# a = 444.178
 
 # 3
 
